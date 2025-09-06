863312f9366137b4b618f0a5bc85f12397e08777
Joel Salse <dectacubito@gmail.com>
2025-09-06 22:50:30 +0200

    new change

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"hMW7F1gaGBPaxc95A3SEnHPzJINnhQInLKgcfarronw=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
